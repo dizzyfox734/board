700af1850b91cba64403a263fe9ad3cd70433432
JE <jechoi337@gmail.com>
2022-04-20 16:17:28 +0900

    Start with Zeplin

--- index.php
+++ index.php
@@ -6,6 +6,27 @@
         <title>Board</title>
     </head>
     <body>
+        <header>
+            <img src="img/2-1.png"
+                srcset="img/2-1@2x.png 2x, img/2-1@3x.png 3x"
+                class="banner">
+        </header>
+
+        <div class="body"> <!-- 좌우위치조절 -->
+            <!-- (아이콘) 방명록 -->
+            <div>
+                <img src="img/userIcon.png"
+                    srcset="img/userIcon@2x.png 2x, img/userIcon@3x.png 3x"
+                    class="userIcon">
+                <span class="pageName">방명록</span>
+            </div>
+            
+            <!-- 게시판 table -->
+        </div>
+
+
+
+
         <?php
         $host = "localhost";
         $user = "root";
