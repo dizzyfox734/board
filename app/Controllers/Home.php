d3d431c973b90f5b65257e1d73071a5d93361d45
JE <jechoi337@gmail.com>
2022-04-29 13:54:48 +0900

    Fix main page

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,8 @@ class Home extends BaseController
 {
     public function index()
     {
-		return view('welcome_message');
+		// return view('welcome_message');
+        return view('/home/firstPage');
     }
 
     public function firstPage()
