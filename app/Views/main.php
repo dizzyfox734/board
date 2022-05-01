63fa0e2bcedb490136852267a12b584148458b1d
dizzyfox734 <dizzyfox734@gmail.com>
2022-05-01 23:02:48 +0900

    Fix table data

--- app/Views/main.php
+++ app/Views/main.php
@@ -18,6 +18,22 @@
                 <div class="userIcon-img"></div>
                 <h2 class="m-0">방명록</h2>
             </div>
+			<?php
+            $host = "localhost";
+            $user = "phpmyadmin";
+            $pw = "asdf1234";
+            $db = "GUESTBOOKDB";
+
+            $conn = mysqli_connect($host, $user, $pw, $db) or die("connect failed.");
+
+            $query = "SELECT * FROM `BOARD_TB` WHERE `DEL_FL` = 'N' ORDER BY NO DESC";
+            $result = mysqli_query($conn, $query);
+
+			if( ($total = mysqli_num_rows($result))>0 ) {
+			} else {
+				echo "NO RECORDS FOUND!";
+			}
+            ?>
             <div class="table-container">
             	<table class="board">
                     <thead>
@@ -29,51 +45,24 @@
                             <td>조회수</td>
                         </tr>
                     </thead>
+					<tbody>
+					<?php
+					while ($rows = mysqli_fetch_assoc($result)) {   ?>
+						<tr>
+							<?php
+							$no = $rows['NO'];
+							$link = "location.href='../context.php?no=$no'";
+							?>
+							<td class="no"><?php echo $no?></td>
+							<td class="title" onClick=<?php echo $link?>><?php echo $rows['TITLE'] ?></td>
+							<td class="writer"><?php echo $rows['ID']?></td>
+							<td class="date"><?php echo $rows['REG_YMD']?></td>
+							<td class="views"><?php echo $rows['VIEW_CNT']?></td>
+						</tr>   <?php  
+					}   ?>
+					</tbody>
                 </table>
             </div>
-
-
-
-            <?php
-            $host = "localhost";
-            $user = "phpmyadmin";
-            $pw = "asdf1234";
-            $db = "GUESTBOOKDB";
-
-            $conn = mysqli_connect($host, $user, $pw, $db) or die("connect failed." . mysql_error());
-
-            $query = "SELECT * FROM `BOARD_TB` WHERE `DEL_FL` = 'N' ORDER BY NO DESC";
-            $result = mysqli_query($conn, $query);
-
-			if( ($total = mysqli_num_rows($result))>0 ) {
-			} else {
-				echo "NO RECORDS FOUND!";
-			}
-            ?>
-            <table class="board">
-                <thead>
-                    <tr>
-                        <td>번호</td>
-                        <td>제목</td>
-                        <td>작성일</td>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php
-                while ($rows = mysqli_fetch_assoc($result)) {   ?>
-                    <tr>
-                        <?php
-                        $no = $rows['NO'];
-                        $link = "location.href='../context.php?no=$no'";
-                        ?>
-                        <td class="no"><?php echo $no?></td>
-                        <td class="title" onClick=<?php echo $link?>><?php echo $rows['TITLE'] ?></td>
-                        <td class="date"><?php echo $rows['REG_YMD']?></td>
-                    </tr>   <?php  
-                }   ?>
-                </tbody>
-            </table>
-            
             <div>
                 <button type="button" onClick="location.href='../write.php'">write</button>
             </div>
