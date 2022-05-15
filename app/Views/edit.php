1eed6509bf16e0e85efe736367dcd1d6efbe561f
dizzyfox734 <dizzyfox734@gmail.com>
2022-05-15 16:00:12 -0700

    Fix edit  (not complete)

--- app/Views/edit.php
+++ app/Views/edit.php
@@ -6,11 +6,11 @@
         <table class="board board-purple-top">
             <tr>
                 <th class="b-head th-grey">제목</th>
-                <th colspan="3"><input class="txt-round w-95" type="text" name="title" autocomplete="off" value=""></th>
+                <th colspan="3"><input class="txt-round w-95" type="text" name="title" autocomplete="off" value="<?= isSet($content) ? $content->title : '' ?>"></th>
             </tr>
             <tr>
                 <th class="th-grey">내용</th>
-                <th colspan="3"><textarea class="txt-round w-95" name="content" rows="15"><?= isSet($content) ? $content->text : '' ?></textarea></th>
+                <th colspan="3"><textarea class="txt-round w-95" name="content" rows="15"><?= isSet($content) ? $content->content : '' ?></textarea></th>
             </tr>
             <!-- 새 글 등록 시에만 보임 -->
             <?= isSet($content) ? '' : '<tr>
@@ -43,8 +43,7 @@
 </div>
 <div class="d-flex align-items-center justify-content-center">
     <a class="no-deco btn btn-white" href="/home/main">취소</a>
-    <!-- <button class="btn btn-purple" onClick="">완료</button> -->
-    <button class="btn btn-purple" onClick=<?= isSet($content) ? `edit($content->id)` : 'edit()' ?>>완료</button>
+    <button class="btn btn-purple" onClick='edit(<?= isSet($content) ? $content->id : "" ?>)' >완료</button>
 </div>
 
 <script src="/assets/js/edit.js"></script>
